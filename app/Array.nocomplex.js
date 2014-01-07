a30154d2b2b4b5158a798a538f716bb53af2a7d9
cyril agosta <cyril.agosta@gmail.com>
2014-01-07 15:47:56 +0100

    Releasing v0.0.18

--- app/Array.nocomplex.js
+++ app/Array.nocomplex.js
@@ -1,5 +1,5 @@
 /**
- * Array.nocomplex version: "0.0.17" Copyright (c) 2011-2012, Cyril Agosta ( cyril.agosta@gmail.com) All Rights Reserved.
+ * Array.nocomplex version: "0.0.18" Copyright (c) 2011-2012, Cyril Agosta ( cyril.agosta@gmail.com) All Rights Reserved.
  * Available via the MIT license.
  * see: http://github.com/cagosta/Array.nocomplex for details
  */
